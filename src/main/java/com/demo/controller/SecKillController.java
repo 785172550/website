2b08440f086d541babe3543f519e56b0424fcc9c
kenneth <785172550@qq.com>
2018-05-19 13:45:39 +0800

    build

--- src/main/java/com/demo/controller/SecKillController.java
+++ src/main/java/com/demo/controller/SecKillController.java
@@ -3,7 +3,6 @@ package com.demo.controller;
 import com.demo.domain.model.Item;
 import com.demo.service.SecKillService;
 import com.google.common.collect.Lists;
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import sun.rmi.runtime.Log;
+
 
 @Controller
 public class SecKillController {
