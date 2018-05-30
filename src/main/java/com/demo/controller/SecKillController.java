9d205c18868141f82cd6de921dbe4fb85bde6989
kenneth <785172550@qq.com>
2018-05-30 23:25:14 +0800

    build

--- src/main/java/com/demo/controller/SecKillController.java
+++ src/main/java/com/demo/controller/SecKillController.java
@@ -16,36 +16,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SecKillController {
 
-  @Autowired
-  SecKillService secKillService;
+    @Autowired
+    SecKillService secKillService;
 
-  @RequestMapping(value = "list", method = RequestMethod.GET)
-  public String getList(Model model) {
-    Item item = new Item(1L, "hello", "content", 10, new Date(), new Date(), new Date());
-    List list = Lists.newArrayList(item);
-    model.addAttribute("list", list);
-    return "list";
-  }
-
-  @RequestMapping("/{Id}/detail")
-  public String getDetail(@PathVariable("Id") Long Id, Model model) {
-    if (Id == null) {
-      return "redirect:/list";
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String getList(Model model) {
+        Item item = new Item(1L, "hello", "content", 10, new Date(), new Date(), new Date());
+        List list = Lists.newArrayList(item);
+        model.addAttribute("list", list);
+        return "list";
     }
-    Item item = secKillService.getById(Id);
-    if (item == null) {
-      return "forward:/list";
+
+    @RequestMapping("/{Id}/detail")
+    public String getDetail(@PathVariable("Id") Long Id, Model model) {
+        if (Id == null) {
+            return "redirect:/list";
+        }
+        Item item = secKillService.getById(Id);
+        if (item == null) {
+            return "forward:/list";
+        }
+        model.addAttribute("item", item);
+        return "detail";
     }
-    model.addAttribute("item", item);
-    return "detail";
-  }
 
-  @RequestMapping("add/{Id}")
-  public Object addItem(@PathVariable("Id") Long Id){
+    @RequestMapping("add/{Id}")
+    public Object addItem(@PathVariable("Id") Long Id) {
 
-    Item item = new Item(Id,"q","content",2,new Date(),new Date(),new Date());
+        Item item = new Item(Id, "q", "content", 2, new Date(), new Date(), new Date());
 
-    return item;
-  }
-
+        return item;
+    }
 }
